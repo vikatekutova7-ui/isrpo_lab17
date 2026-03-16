03102a5f12439026eaef938be64c9e7198065374
vikatekutova7-ui <vikatekutova7@gmail.com>
2026-03-16 21:38:36 +0300

    Task 3

---
+++ code/Task3_GuessGame/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите число от 1 до 100: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int s = 0;
+Random rand = new Random();
+int r = rand.Next(1, 101);
+while (s == 0)
+{
+    if (a < r)
+    {
+        Console.WriteLine("меньше загаданного");
+        Console.Write("Введи еще раз: ");
+         a = Convert.ToInt32(Console.ReadLine());
+    }
+    else if (a > r)
+    {
+        Console.WriteLine("больше загаданного");
+        Console.Write("Введи еще раз: ");
+         a = Convert.ToInt32(Console.ReadLine());
+    }
+    else { Console.WriteLine("угадал");s = 1; }
+}
